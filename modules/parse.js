c34d966be76d8cf023b770c26cf769fc165e23ae
riteck <dmitriy.sychev@utexas.edu>
2019-10-03 16:25:20 -0500

    Chaned parse

--- modules/parse.js
+++ modules/parse.js
@@ -4,10 +4,32 @@ module.exports.kolesaParse = (url) => {
         const $ = cheerio.load(body);
         let numPages = $('.pager')['0'].children[1].children[$('.pager')['0'].children[1].children.length-2].children[0]
         .children[0].children[0].data;
-        //console.log($('script')[10]); // string
-        let dataScript = $('script')[10].children[0].data.substring('listing.item.push'.length + 7, $('script')[10].children[0].data.length - 3);
-        dataScript = JSON.parse(dataScript); // json запарсенный 
-        //console.log(dataScript);
+        
+        /*добавлйет машины в массив */
+        // let posts = [];
+        // for (let i = 10; i < 35; i++) {
+        //     posts[i-10] = $('script')[i];
+        // }
+
+        // // убрать рекламу и оставить только машины
+        
+        // posts = posts.filter(word => isListingOnly(word.children[0].data));
+        
+        let cars = Array($('script'));
+        console.log(cars.length);
+        cars = (word => isListingOnly(word.children[0].data));
+
+        // let cars = [];
+        // let dataScript = $('script')[10].children[0].data.substring('listing.item.push'.length + 7, $('script')[10].children[0].data.length - 3);
+        // dataScript = JSON.parse(dataScript); // json запарсенный 
+        // console.log(dataScript);
+
         //console.log(`this is\n${dataScript.region}`);
     });
+
+
+    function isListingOnly(val)  {
+        return val.includes('listing');
+    }
+
 };

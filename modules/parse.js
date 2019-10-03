fab4ebe6aa828e63b74408037283c3772eca63ae
riteck <dmitriy.sychev@utexas.edu>
2019-10-03 17:10:44 -0500

    Formatted cars as json array

--- modules/parse.js
+++ modules/parse.js
@@ -5,31 +5,34 @@ module.exports.kolesaParse = (url) => {
         let numPages = $('.pager')['0'].children[1].children[$('.pager')['0'].children[1].children.length-2].children[0]
         .children[0].children[0].data;
         
-        /*добавлйет машины в массив */
-        // let posts = [];
-        // for (let i = 10; i < 35; i++) {
-        //     posts[i-10] = $('script')[i];
-        // }
-
-        // // убрать рекламу и оставить только машины
+        /*добавляет объявления в массив */
+        let cars = [];
+        Object.keys($('script')).forEach((key) => {
+            cars.push($('script')[key]);
+        });
         
-        // posts = posts.filter(word => isListingOnly(word.children[0].data));
+        /*фильтруем так чтобы в массиве были только машины*/
+        cars = cars.filter((car) => {
+            if (car.hasOwnProperty('children')) {
+                if (car.children.length > 0) {
+                    car = car.children[0];
+                    if (car.hasOwnProperty('data')) {
+                        car = car.data;
+                        return car.includes('listing.items.push');
+                    }
+                }
+            }
+            return false;
+        });
         
-        let cars = Array($('script'));
-        console.log(cars.length);
-        cars = (word => isListingOnly(word.children[0].data));
+        /*парсим в нужный формат */
+        let carsJson = [];
 
-        // let cars = [];
-        // let dataScript = $('script')[10].children[0].data.substring('listing.item.push'.length + 7, $('script')[10].children[0].data.length - 3);
-        // dataScript = JSON.parse(dataScript); // json запарсенный 
-        // console.log(dataScript);
+        cars.forEach((car) => {
+            carsJson.push(JSON.parse(car.children[0].data.substring(car.children[0].data.indexOf('{'), car.children[0].data.length - 3)));
+        });
 
-        //console.log(`this is\n${dataScript.region}`);
     });
 
-
-    function isListingOnly(val)  {
-        return val.includes('listing');
-    }
 
 };

139b14f47c3168df016405739f3ad3462b3f0cc3
riteck <dmitriy.sychev@utexas.edu>
2019-10-03 23:48:27 -0500

    Ubral nenuzhnoe

--- modules/parse.js
+++ modules/parse.js
@@ -1,5 +1,5 @@
 
-module.exports.kolesaParse = function(url) {
+module.exports.kolesaParse = async function(url) {
 
     
     function getNumPages(url) {
@@ -17,68 +17,44 @@ module.exports.kolesaParse = function(url) {
         let promises = [];
         let carsJson = [];
         for (let i = 1; i < numPages; i++) {
-            promises.push(request(url+`&page=${i}`, (error, response, body) => {
-                const $ = cheerio.load(body);
-                
-                /*добавляет объявления в массив */
-                let cars = [];
-                Object.keys($('script')).forEach((key) => {
-                    cars.push($('script')[key]);
-                });
-                
-                /*фильтруем так чтобы в массиве были только машины*/
-                cars = cars.filter((car) => {
-                    if (car.hasOwnProperty('children')) {
-                        if (car.children.length > 0) {
-                            car = car.children[0];
-                            if (car.hasOwnProperty('data')) {
-                                car = car.data;
-                                return car.includes('listing.items.push');
+            promises.push(
+                request(url+`&page=${i}`, (error, response, body) => {
+                    const $ = cheerio.load(body);
+                    
+                    /*добавляет объявления в массив */
+                    let cars = [];
+                    Object.keys($('script')).forEach((key) => {
+                        cars.push($('script')[key]);
+                    });
+                    
+                    /*фильтруем так чтобы в массиве были только машины*/
+                    cars = cars.filter((car) => {
+                        if (car.hasOwnProperty('children')) {
+                            if (car.children.length > 0) {
+                                car = car.children[0];
+                                if (car.hasOwnProperty('data')) {
+                                    car = car.data;
+                                    return car.includes('listing.items.push');
+                                }
                             }
                         }
+                        return false;
+                    });
+                    
+                    /*парсим в нужный формат */
+                    
+                    cars.forEach((car) => {
+                        carsJson.push(JSON.parse(car.children[0].data.substring(car.children[0].data.indexOf('{'), car.children[0].data.length - 3)));
+                    });
+                
                     }
-                    return false;
-                });
-                
-                /*парсим в нужный формат */
-                
-                cars.forEach((car) => {
-                    carsJson.push(JSON.parse(car.children[0].data.substring(car.children[0].data.indexOf('{'), car.children[0].data.length - 3)));
-                });
-                
-            }));
+                )
+            );
             //resolve(carsJson);
         } //end of for loop
         Promise.all(promises)
         .then((results) => {
-            Object.keys(results).forEach((key) => {
-                const $ = cheerio.load(key);
-
-                let cars = [];
-                Object.keys($('script')).forEach((key) => {
-                    cars.push($('script')[key]);
-                });
-                
-                /*фильтруем так чтобы в массиве были только машины*/
-                cars = cars.filter((car) => {
-                    if (car.hasOwnProperty('children')) {
-                        if (car.children.length > 0) {
-                            car = car.children[0];
-                            if (car.hasOwnProperty('data')) {
-                                car = car.data;
-                                return car.includes('listing.items.push');
-                            }
-                        }
-                    }
-                    return false;
-                });
-                
-                /*парсим в нужный формат */
-                
-                cars.forEach((car) => {
-                    carsJson.push(JSON.parse(car.children[0].data.substring(car.children[0].data.indexOf('{'), car.children[0].data.length - 3)));
-                });
-            });
+            console.log(carsJson.length);
             return carsJson;
         })
     }).catch(err => {
